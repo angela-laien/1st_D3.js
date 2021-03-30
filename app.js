9cf2a038e3c358f9cdcf410d2463149c8739e782
angela-laien <laienxie@gmail.com>
2021-03-29 20:45:40 -0700

    bar graph with dummy data

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ const DUMMY_DATA = [
     { id: 'd2', value: 11, region: 'India' },
     { id: 'd3', value: 12, region: 'China' },
     { id: 'd4', value: 16, region: 'Germany' },
-]
+];
 
-d3.select('div')
-  .selectAll('p')
-  .data([1,2,3])
+const xScale = d3
+  .scaleBand()
+  .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+  .rangeRound([0, 250])
+  .padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 20]).range([200, 0]);
+
+const container = d3.select('svg').classed('container', true);
+
+const bars = container
+  .selectAll('.bar')
+  .data(DUMMY_DATA)
   .enter()
-  .append('p')
-  .text(dta => dta);
+  .append('rect')
+  .classed('bar', true)
+  .attr('width', xScale.bandwidth())
+  .attr('height', (data) => 200-yScale(data.value))
+  .attr('x', data => xScale(data.region))
+  .attr('y', data => yScale(data.value));
+
+setTimeout(() => {
+  bars.data(DUMMY_DATA.slice(0, 2)).exit().remove();
+}, 2000);
